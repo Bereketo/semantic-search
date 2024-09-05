84d0e902320d95c050875d54476beced0eda0766
Berekto <bereketalebachew87@gmail.com>
2024-09-05 07:20:11 +0300

    update index

--- faiss_index.py
+++ faiss_index.py
@@ -3,9 +3,17 @@ import numpy as np
 
 
 def build_faiss_index(embeddings):
-    """Build and return faiss index"""
+    """Build and return faiss IVF index"""
     dimensions = embeddings[0].shape[0]
-    index = faiss.IndexFlatL2(dimensions)
+    nlist = 30
+    quantizer = faiss.IndexFlatL2(dimensions)
+    index = faiss.IndexIVFFlat(quantizer, dimensions, nlist, faiss.METRIC_L2)
+
     embeddings_array = np.array(embeddings.tolist())
+
+    assert not index.is_trained
+    index.train(embeddings_array)
+    assert index.is_trained
+
     index.add(embeddings_array)
     return index
